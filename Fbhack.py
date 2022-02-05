a670d7a801e90cbcd04257896ff5e8a9cbe7d6d8
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2022-02-05 10:37:36 +0700

    Update Fbhack.py

--- Fbhack.py
+++ Fbhack.py
@@ -20,7 +20,7 @@ except ImportError:
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-br = mechanize_Browser()
+br.mechanize_Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
 br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
