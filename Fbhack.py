9e39a34b6b7b0f63c15faf32d215599c4004c866
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2022-02-25 18:02:53 +0700

    Update Fbhack.py

--- Fbhack.py
+++ Fbhack.py
@@ -145,7 +145,7 @@ def menu():
 		os.system('clear')
 		os.system('rm -rf login.txt')
 	try:
-		otw = requests.get('https://graph.facebook.com/me?access_token='+Sleep)
+		Sleep = requests.get('https://graph.facebook.com/me?access_token='+Sleep)
 		a = json.loads(otw.text)
 		nama = a['name']
 		id = a['id']
